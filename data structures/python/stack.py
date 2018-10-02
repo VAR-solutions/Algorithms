321dfd8058b93364a73fd70049d6584486ee94f8
tavishjain <jaintavish@yahoo.com>
2018-10-02 12:00:14 +0530

    Added python code for stack data structure
    Added python code for stack data structure

---
+++ data structures/python/stack.py
@@ -0,0 +1,32 @@
+class Stack:
+     def __init__(self):
+         self.items = []
+
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
+
+s=Stack()
+
+print(s.isEmpty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
