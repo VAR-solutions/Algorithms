a6f512470cf8e06a1474ee47e4f499201a89ec03
Kyler Stole <kylerstole@gmail.com>
2018-10-02 23:01:45 -0700

    Add fibonacci in JavaScript

---
+++ Dynamic Programming/fibonacci/java/fibonacci.js
@@ -0,0 +1,18 @@
+function main() {
+  int n = 9;
+  console.log("Fib(" + n + "): " + fib(n));
+}
+
+function fib(n) {
+  if (n === 0) return 0; // Special case for 0
+
+  // Initial values of fibonacci series
+  var f = [1, 1];
+
+  for (var i = 2; i <= n; i++) {
+    // Use modulus to switch array indexes
+    f[i%2] = f[0] + f[1];
+  }
+
+  return f[(n+1)%2];
+}
