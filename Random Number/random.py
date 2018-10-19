b6643e01e1831575ab0100360f46703880d6d57a
Vikrant Srivastava <sri.vkrnt@gmail.com>
2018-10-20 04:11:23 +0530

    Adding random number generation using python

---
+++ Random Number/random.py
@@ -0,0 +1,7 @@
+#Way to generate random numbers in python
+# random.randint(l,r+1) will generate a random number between l and r
+
+
+import random
+for x in range(10):
+  print random.randint(1,101)
