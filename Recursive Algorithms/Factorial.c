1a720cae183c2c9685aba41434bd178cd3721a03
Gauri Vaidya <gsvaidya2608@gmail.com>
2018-10-19 20:08:53 +0530

    Create Factorial.c
    A recursive Factorial program

---
+++ Recursive Algorithms/Factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int num, ans;
+	int fact=1,i=1;
+	int factorial(int n);
+
+	printf("Enter number whose factorial is to be found\n");
+	scanf("%d",&num);
+	ans=factorial(num);
+	printf("factorial is %d",ans);
+
+	return 0;
+}
+int factorial(int n){
+	int res=n;
+	if (res>1){
+		res*=factorial(n-1);
+	}
+	return res;
+}
