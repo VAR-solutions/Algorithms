0a3b09808a7f9fb01bbde213231c028822132f81
fopeak <fopeak@gmail.com>
2018-10-21 01:31:42 +0300

    Linear search in go

---
+++ Searching/Linear Search/go/linear_search.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	array := []int32{23, 42, 65, 75, 92, 75, 21, 56, 32, 93}
+	status := search(array, 42)
+	if status == "not found" {
+		fmt.Println("not found.")
+	} else {
+		fmt.Println(status)
+	}
+}
+
+func search(array []int32, target int32) string {
+	for i := range array {
+		if array[i] == target {
+			return "found at " + strconv.Itoa(i) + ""
+		}
+	}
+	return "not found"
+}
