3c2e1968fa2b1698356bb5fdf5c7811a619492d8
picollo <lucaspicolloo@gmail.com>
2018-10-20 20:15:13 +0000

    logic fix

--- data structures/queue/c/queue_using_array.c
+++ data structures/queue/c/queue_using_array.c
@@ -3,55 +3,37 @@
 
 #define MAX 500
 
-struct Queue
+typedef struct Queue
 {
     int size;
     int *a;
     int front;
     int rear;
-};
+} queue; 
 
-typedef struct Queue queue;
-
-queue *createQueue()
+queue* createQueue()
 {
     queue *q = malloc(sizeof(queue));
     q->size = 0;
     q->front = 0;
     q->rear = 0;
-    q->a = malloc(MAX * sizeof(int));
+    q->a = calloc(MAX, sizeof(int));
 }
 
 int isEmpty(queue *q)
 {
-    if (q->size == 0)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+    return q->size == 0;
 }
 
 int isFull(queue *q)
 {
-    if (q->size == MAX)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+    return q->size == MAX;
 }
 
 void enqueue(queue *q, int n)
 {
-    if (isFull(q))
-    {
-        printf("Queue is full.\n");
-    }
+    if (isFull(q))    
+        printf("Queue is full.\n");    
     else
     {
         (q->rear)++;
@@ -63,10 +45,8 @@ void enqueue(queue *q, int n)
 
 void dequeue(queue *q)
 {
-    if (isEmpty(q))
-    {
-        printf("Queue is empty.\n");
-    }
+    if (isEmpty(q))    
+        printf("Queue is empty.\n");    
     else
     {
         printf("%d dequeued from the queue.\n", q->a[++q->front]);
@@ -76,34 +56,28 @@ void dequeue(queue *q)
 
 int peek(queue *q)
 {
-    if (isEmpty(q))
-    {
-        printf("Queue is empty.\n");
-    }
-    else
-    {
-        return q->a[q->front];
-    }
+    if (isEmpty(q))    
+        printf("Queue is empty.\n");    
+    else    
+        return q->a[q->front];    
 }
 
 void display(queue *q)
 {
-    int i = 0;
-    for (i = q->front + 1; i < q->rear + 1; i++)
-    {
+    for (int i = q->front + 1; i < q->rear + 1; i++)    
         printf("%d\t", q->a[i]);
-    }
+
     printf("\n");
 }
 
 int main()
 {
     queue *q = createQueue();
-    int c = 0;
-    printf("Enter 1 to enqueue elements in queue.\nEnter 2 to dequeue element from the queue.\nEnter 3 to peek topmost element in queue.\nEnter 4 to display content of the queue.\n");
-    scanf("%d", &c);
+    int c = 0;    
     do
     {
+        printf("\nEnter 1 to enqueue elements in queue.\nEnter 2 to dequeue element from the queue.\nEnter 3 to peek topmost element in queue.\nEnter 4 to display content of the queue.\n");
+        scanf("%d", &c);
         switch (c)
         {
         case 1:
@@ -124,7 +98,6 @@ int main()
         default:
             printf("Invalid input.\n");
         }
-        scanf("%d", &c);
     } while (c != -1);
     return 0;
 }
