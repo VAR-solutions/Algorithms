4e9c360c44917a87e04e2acc4c0eb1366473f717
Petar Petkovic <petkpera@gmail.com>
2018-10-20 18:42:20 +0200

    fixed all compiler errors

--- Sorting/Selection Sort/cpp/selectionsort.cpp
+++ Sorting/Selection Sort/cpp/selectionsort.cpp
@@ -1,33 +1,28 @@
-#include<iostream.h>
-#include<conio.h>
-void main()
+#include<iostream>
+using namespace std;
+
+int main()
 {
-	clrscr();
-	int size, arr[50], i, j, temp;
-	cout<<"Enter Array Size : ";
-	cin>>size;
-	cout<<"Enter Array Elements : ";
-	for(i=0; i<size; i++)
-	{
-		cin>>arr[i];
+	int size, arr[50];
+	cout << "Enter Array Size: ";
+	cin >> size;
+	cout << "Enter Array Elements: ";
+	for(int i = 0; i < size; i++) {
+		cin >> arr[i];
 	}
-	cout<<"Sorting array using selection sort...\n";
-	for(i=0; i<size; i++)
-	{
-		for(j=i+1; j<size; j++)
-		{
-			if(arr[i]>arr[j])
-			{
-				temp=arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
+
+	cout << "Sorting array using Selection Sort...\n";
+	for(int i = 0; i < size; i++) {
+		for(int j = i+1; j < size; j++) {
+			if(arr[i] > arr[j]) {
+				swap(arr[i], arr[j]);
 			}
 		}
 	}
-	cout<<"Now the Array after sorting is :\n";
-	for(i=0; i<size; i++)
-	{
-		cout<<arr[i]<<" ";
+	cout << "Now the Array after sorting is :\n";
+	for(int i=0; i<size; i++) {
+		cout << arr[i] << " ";
 	}
-	getch();
+
+	return 0;
 }
