08de3372141f8da178560a4b1255069fbf74a92c
Emily Gong <z.gong@alumni.ubc.ca>
2018-10-21 19:59:38 -0700

    Revert "Revert "Revert "Fixed issues in CountSet"""
    This reverts commit b6b4f0d74dc0c95f0b59549a9171c7f58768ebb4.

--- Bit Manipulation/countSet/cpp/Count-set-bits.cpp
+++ Bit Manipulation/countSet/cpp/Count-set-bits.cpp
@@ -1,15 +1,7 @@
 #include <iostream> 
 using namespace std; 
   
-int main() {
-   cout << "Enter a number: ";
-   cin >> n;
-   while(cin.fail()) {
-       cout << "Invalid input. Please re-enter: " << endl;
-       cin.clear();
-       cin.ignore(256,'\n');
-       cin >> n;
-   }
-   cout << __builtin_popcount (n) << "\n";
+int main() { 
+   cout << __builtin_popcount (4) << "\n"; 
    return 0; 
 } 
