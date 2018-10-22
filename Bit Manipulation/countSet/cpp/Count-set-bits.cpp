a1764786630a4539f9ee3115cde1ab2dfc281951
Emily Gong <z.gong@alumni.ubc.ca>
2018-10-21 19:47:34 -0700

    Fixed issues in CountSet

--- Bit Manipulation/countSet/cpp/Count-set-bits.cpp
+++ Bit Manipulation/countSet/cpp/Count-set-bits.cpp
@@ -1,7 +1,15 @@
 #include <iostream> 
 using namespace std; 
   
-int main() { 
-   cout << __builtin_popcount (4) << "\n"; 
+int main() {
+   cout << "Enter a number: ";
+   cin >> n;
+   while(cin.fail()) {
+       cout << "Invalid input. Please re-enter: " << endl;
+       cin.clear();
+       cin.ignore(256,'\n');
+       cin >> n;
+   }
+   cout << __builtin_popcount (n) << "\n";
    return 0; 
 } 
