b25895ecbf6a15f44dd85f93db5d23d845971556
eashsaxena <44508862+eashsaxena@users.noreply.github.com>
2018-10-27 00:46:30 +0530

    updated code

--- Dynamic Programming/BellmanFord-SingleSourceShortestPath.c
+++ Dynamic Programming/BellmanFord-SingleSourceShortestPath.c
@@ -70,25 +70,4 @@ void printSolution(int dist[][V])
     } 
 } 
   
-// driver program to test above function 
-int main() 
-{ 
-    /* Let us create the following weighted graph 
-            10 
-       (0)------->(3) 
-        |         /|\ 
-      5 |          | 
-        |          | 1 
-       \|/         | 
-       (1)------->(2) 
-            3           */
-    int graph[V][V] = { {0,   5,  INF, 10}, 
-                        {INF, 0,   3, INF}, 
-                        {INF, INF, 0,   1}, 
-                        {INF, INF, INF, 0} 
-                      }; 
-  
-    // Print the solution 
-    floydWarshall(graph); 
-    return 0; 
-} 
+
