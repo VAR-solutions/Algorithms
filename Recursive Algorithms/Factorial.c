06cdd13b9cb68f75bd4dc8939f83fed4f0344445
eashsaxena <44508862+eashsaxena@users.noreply.github.com>
2018-10-27 00:34:07 +0530

    added recursive function in Factorial.c

--- Recursive Algorithms/Factorial.c
+++ Recursive Algorithms/Factorial.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
-
+long int multiplyNumbers(int n);
 
 int main()
 {
-	int num, ans;
-	int fact=1,i=1;
-	int factorial(int n);
-
-	printf("Enter number whose factorial is to be found\n");
-	scanf("%d",&num);
-	ans=factorial(num);
-	printf("factorial is %d",ans);
-
-	return 0;
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    printf("Factorial of %d = %ld", n, multiplyNumbers(n));
+    return 0;
 }
-int factorial(int n){
-	int res=n;
-	if (res=0|| res=1) return 1;
-	if (res>1){
-		res*=factorial(n-1);
-	}
-	return res;
+long int multiplyNumbers(int n)
+{
+    if (n >= 1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
+}
 }
