c48c81664ff6008cc5966846a330b63277e1faca
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-10-27 13:20:13 +0530

    Enhanced the logic and optimized it

--- Mathematics/factorial/javascript/factorial.js
+++ Mathematics/factorial/javascript/factorial.js
@@ -1,13 +1,14 @@
-function factorial (num) {
-    if (num < 0 ) {
-        return
-    } 
-    
-    if (num === 1) {
-        return 1;
-    }
+function factorial(num) {
+    if (num < 0 ) throw new Error("Negative numbers aren't allowed")
+    if (num % 1 !== 0 ) throw new Error("Number should be an integer")
 
-    return num*factorial(num-1)
+    // Seperating the test logic from core logic
+    
+    function compute(num) {
+      if (num === 1 || num === 0) return 1
+      return num * compute(num -1)
+    }
+    return compute(num)
 }
 
 console.log(factorial(5))
