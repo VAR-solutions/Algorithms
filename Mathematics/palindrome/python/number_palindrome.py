69ddba284ab3b7f75efb32bc816aa470f43cff1d
ROHIT KUMAR <rkrohitkmr93@gmail.com>
2018-10-27 18:04:37 +0530

    Update number_palindrome.py

--- Mathematics/palindrome/python/number_palindrome.py
+++ Mathematics/palindrome/python/number_palindrome.py
@@ -11,3 +11,6 @@ if num == rev:
     print("number is palindrome")
 else:
     print("number is not palindrome")
+  
+#slicing of string can also be used to get a reverse
+#of the number to check whether it is palindrome or not.
