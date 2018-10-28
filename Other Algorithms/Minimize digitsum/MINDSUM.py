a3e2149d55a381ffca198cd70219d8dd4458d647
prateeksharma21a <prateeksharma21abc@gmail.com>
2018-10-28 11:55:31 +0530

    Minimize Digitsum

---
+++ Other Algorithms/Minimize digitsum/MINDSUM.py
@@ -0,0 +1,16 @@
+def digitsum(N):
+    d=list(str(N))
+    l2=[int(i) for i in s]
+    return(sum(l2))
+
+def solve():
+
+
+
+def main():
+    T=int(input())
+    for _ in range(T):
+        N,D=map(int,input().split())
+
+if __name__=='__main__':
+    main()
