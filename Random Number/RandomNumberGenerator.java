f5c8a19d8c0830f0ef3a88436fe1e6adf9e4fd3d
Indra Bhan Singh Patel <ibsp91096@gmail.com>
2018-10-28 19:42:28 +0530

    Added a code to generate random number in java

---
+++ Random Number/RandomNumberGenerator.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class RandomNumberGenerator {
+
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        // make a random number within range of 500
+        int randomInt = random.nextInt(500);
+        System.out.println(randomInt);
+
+        // makes random integer without any out bound
+        int randomInt2 = random.nextInt();
+        System.out.println(randomInt2);
+
+        // make a random number without any out bound
+        double randomDouble = random.nextDouble();
+        System.out.println(randomDouble);
+
+    }
+
+
+}
