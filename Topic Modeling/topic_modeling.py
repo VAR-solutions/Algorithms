373a83c552c24c47f39201966a81c164e7cb1627
djmv <dayanm@uninorte.edu.co>
2018-10-28 22:28:34 -0500

    Add topic modeling script
    Topic modeling is ML algorithm for automatic topic detection

---
+++ Topic Modeling/topic_modeling.py
@@ -0,0 +1,45 @@
+"""
+Expample of Topic modeling for the 20 Newsgroups dataset
+Two methods are used LDA and NMF 
+choose what is the best. 
+"""
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.decomposition import NMF, LatentDirichletAllocation
+
+def display_topics(model, feature_names, no_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print "Topic %d:" % (topic_idx)
+        print " ".join([feature_names[i]
+                        for i in topic.argsort()[:-no_top_words - 1:-1]])
+
+# Load dataset of news 
+dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
+documents = dataset.data
+#Max number of features 
+no_features = 1000
+
+# NMF is able to use tf-idf
+tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
+tfidf = tfidf_vectorizer.fit_transform(documents)
+tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+
+# LDA can only use raw term counts for LDA because it is a probabilistic graphical model
+tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
+tf = tf_vectorizer.fit_transform(documents)
+tf_feature_names = tf_vectorizer.get_feature_names()
+
+#Number of topic desired
+no_topics = 20 
+
+# Run NMF
+nmf = NMF(n_components=no_topics, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(tfidf)
+
+# Run LDA
+lda = LatentDirichletAllocation(n_topics=no_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(tf)
+
+no_top_words = 10
+#Display topics extracted with NMF method
+display_topics(nmf, tfidf_feature_names, no_top_words)
+#Display topics extracted with LDA method
+display_topics(lda, tf_feature_names, no_top_words)
