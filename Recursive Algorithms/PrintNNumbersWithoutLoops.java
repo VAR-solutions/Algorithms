3e69ba2a5a5327f896a6e6e9a7ae81cd09f5756f
Maruthi <19310076+MaruthiMohanReddyPutha@users.noreply.github.com>
2018-10-29 22:58:58 -0400

    fixed the static variable and method

--- Recursive Algorithms/PrintNNumbersWithoutLoops.java
+++ Recursive Algorithms/PrintNNumbersWithoutLoops.java
@@ -3,13 +3,13 @@
     We use recursion logic to print N numbers using a generic function
  */
 public class PrintNumbersWithoutLoops {
-    public int N;
+    public static int N;
     public static void main(String[] args) {
         N = 100; // the number of natural numbers to be printed
         printNumbers(1); // prints N natural numbers starting from 1
     }
 
-    public void printNumbers(int givenNumber) {
+    public static void printNumbers(int givenNumber) {
         if(givenNumber < N) {
             System.out.println(givenNumber);
             printNumber(++givenNumber);
