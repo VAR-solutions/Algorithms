ea516c827d727de61c07fe8e19bcd20e3e4e126b
Felipe Pimenta <fhpimenta12@gmail.com>
2018-10-30 11:37:55 -0300

    Implementation of Linked List in PHP

--- LinkedList/LinkedList.php
+++ LinkedList/LinkedList.php
@@ -2,69 +2,117 @@
 
 declare(strict_types=1);
 
+/**
+ * Class Node
+ */
 class Node
 {
     protected $data;
     protected $next;
 
+    /**
+     * Node constructor.
+     * @param int $data
+     */
     public function __construct(int $data)
     {
         $this->data = $data;
         $this->next = null;
     }
 
-    public function setNext(Node $next)
+    /**
+     * Set next node
+     * @param Node|null $next
+     */
+    public function setNext($next)
     {
         $this->next = $next;
     }
 
+    /**
+     * Get next node
+     * @return Node|null
+     */
     public function getNext()
     {
         return $this->next;
     }
 
+    /**
+     * Set data of Node
+     * @param int $data
+     */
     public function setData(int $data)
     {
         $this->data = $data;
     }
 
+    /**
+     * Get data of Node
+     * @return int
+     */
     public function getData()
     {
         return $this->data;
     }
 }
 
+/**
+ * Class LinkedList
+ *
+ * Implementation of Linked List in PHP
+ */
 class LinkedList
 {
     protected $size;
     protected $head;
 
+    /**
+     * LinkedList constructor.
+     * @param int $data
+     */
     public function __construct(int $data)
     {
         $this->head = new Node($data);
         $this->size = 1;
     }
 
+    /**
+     * Add data to the head of the list
+     *
+     * @param int $data
+     */
     public function addAtHead(int $data)
     {
-        $temp = $this->head;
+        $current = $this->head;
         $this->head = new Node($data);
-        $this->head->setNext($temp);
+        $this->head->setNext($current);
         $this->size++;
     }
 
+    /**
+     * Add data to the end of the list
+     *
+     * @param int $data
+     */
     public function add(int $data)
     {
-        $temp = $this->head;
-        while (!empty($temp->getNext())) {
-            $temp = $temp->getNext();
+        $current = $this->head;
+        while (!empty($current->getNext())) {
+            $current = $current->getNext();
         }
 
-        $temp->setNext(new Node($data));
+        $current->setNext(new Node($data));
         $this->size++;
     }
 
-    public function addAtIndex(int $index, int $data)
+    /**
+     * Add data an specific index of the list
+     *
+     * @param int $data
+     * @param int $index
+     */
+    public function addAtIndex(int $data, int $index)
     {
         $temp = $this->head;
         for ($i = 0; $i < ($index-1); $i++) {
@@ -77,76 +125,185 @@ class LinkedList
         $this->size++;
     }
 
-    public function get($index)
+    /**
+     * Get data of item at index
+     *
+     * @param int $index
+     * @return int|null
+     */
+    public function get(int $index)
     {
         if ($index < 0) {
             return null;
         }
 
-        $nodeCurrent = null;
+        $current = null;
 
         if (!empty($this->head)) {
-            $nodeCurrent = $this->head->getNext();
+            $current = $this->head;
 
             for ($i = 0; $i < $index; $i++) {
-                if (empty($nodeCurrent->getNext())) {
+                if (empty($current->getNext())) {
                     return null;
                 }
 
-                $nodeCurrent = $nodeCurrent->getNext();
+                $current = $current->getNext();
             }
-            return $nodeCurrent->getData();
+
+            return $current->getData();
         }
-        return $nodeCurrent;
+
+        return $current;
     }
 
-    public function pop()
+    /**
+     * Remove head of the list
+     *
+     * @return bool
+     */
+    public function removeHead()
     {
-        $next = null;
+        if (empty($this->head->getNext())) {
+            return false;
+        }
 
         $next = $this->head->getNext();
-
         $this->head = $next;
+        $this->size--;
+
+        return true;
     }
 
+    /**
+     * Remove last item of the list
+     *
+     * @return bool
+     */
+    public function removeLast()
+    {
+        if (empty($this->head->getNext())) {
+            $this->head = null;
+            $this->size--;
+            return true;
+        }
+
+        $current = $this->head;
+        while (!empty($current->getNext()->getNext())) {
+            $current = $current->getNext();
+        }
+
+        $current->setNext(null);
+        $this->size--;
+        return true;
+    }
+
+    /**
+     * Remove a specific item at index
+     *
+     * @param int $index
+     * @return bool
+     */
     public function remove(int $index)
     {
-        $temp = $this->head;
+        if ($index == 0) {
+            return $this->removeHead();
+        }
 
+        if ($index == $this->size-1) {
+            return $this->removeLast();
+        }
+
+        $current = $this->head;
+        for ($i = 0; $i < $index; $i++) {
+            if (empty($current->getNext())) {
+                return false;
+            }
+            $current = $current->getNext();
+        }
+
+        $temp = $current->getNext();
+        $current->setNext($temp->getNext());
+        $this->size--;
+
+        return true;
     }
 
+    /**
+     * Get size of the list
+     *
+     * @return int
+     */
     public function getSize()
     {
         return $this->size;
     }
 
+    /**
+     * Print all itens of the list
+     * 
+     * @return string
+     */
     public function __toString()
     {
         $output = '';
 
-        if (!empty($this->head)) {
-            $nodeCurrent = $this->head->getNext();
-            while (!empty($nodeCurrent)) {
-                $output .= '['. $nodeCurrent->getData(). ']';
-                $nodeCurrent = $nodeCurrent->getNext();
-            }
+        $current = $this->head;
+        while (!empty($current)) {
+            $output .= '['. $current->getData(). ']';
+            $current = $current->getNext();
         }
 
         return $output;
     }
 }
 
-$linkedList = new LinkedList(1);
+echo "Create Linked List in PHP\n\n";
 
-$linkedList->add(1);
+echo "Inserting number 1...\n";
+$linkedList = new LinkedList(1);
 echo $linkedList .PHP_EOL;
+echo "Size: ". $linkedList->getSize() . "\n\n";
+
+echo "Inserting number 3...\n";
+$linkedList->add(3);
+echo $linkedList .PHP_EOL;
+echo "Size: ". $linkedList->getSize() . "\n\n";
+
+echo "Inserting number 5...\n";
 $linkedList->add(5);
 echo $linkedList .PHP_EOL;
-$linkedList->add(8);
+echo "Size: ". $linkedList->getSize() . "\n\n";
+
+echo "Inserting number 8 at index 1...\n";
+$linkedList->addAtIndex(8, 1);
 echo $linkedList .PHP_EOL;
+echo "Size: ". $linkedList->getSize() . "\n\n";
+
+echo "Inserting number 9...\n";
 $linkedList->add(9);
 echo $linkedList .PHP_EOL;
+echo "Size: ". $linkedList->getSize() . "\n\n";
 
-// remove index 2
-$linkedList->remove(2);
+echo "Inserting number 13 at head...\n";
+$linkedList->addAtHead(13);
+echo $linkedList .PHP_EOL;
+echo "Size: ". $linkedList->getSize() . "\n\n";
+
+echo "Removing last item of list...\n";
+$linkedList->removeLast();
+echo $linkedList .PHP_EOL;
+echo "Size: ". $linkedList->getSize() . "\n\n";
+
+echo "Removing first item of list...\n";
+$linkedList->removeHead();
+echo $linkedList .PHP_EOL;
+echo "Size: ". $linkedList->getSize() . "\n\n";
+
+echo "Removing item at index 1...\n";
+$linkedList->remove(1);
 echo $linkedList . PHP_EOL;
+echo "Size: ". $linkedList->getSize() . "\n\n";
+
+echo "Get data of the first item of the list...". PHP_EOL;
+$data = $linkedList->get(0);
+echo "Data: ". $data . PHP_EOL;
