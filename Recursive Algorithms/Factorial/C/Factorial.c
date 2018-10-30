27460070c645cec408000c4579239bfbccc2cf5b
Malik Ahmed <animanmaster@gmail.com>
2018-10-30 03:21:33 -0400

    Move Factorial.c under the Factorial folder

---
+++ Recursive Algorithms/Factorial/C/Factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+long int multiplyNumbers(int n);
+
+int main()
+{
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    printf("Factorial of %d = %ld", n, multiplyNumbers(n));
+    return 0;
+}
+long int multiplyNumbers(int n)
+{
+    if (n >= 1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
+}
+}
