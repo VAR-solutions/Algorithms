98746f4b4f1fa2ad88cf64b8b34f1264313826b0
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-10-30 06:13:28 +0530

    Update fibo.cpp
    previous code calculated fibonacci n+1

--- Dynamic Programming/fibo.cpp
+++ Dynamic Programming/fibo.cpp
@@ -5,6 +5,7 @@ int Fib[10000];
 
 int main() {
 	int n;
+	cout<<"\nEnter n = ";
 	cin >> n;
 
 	//0th and 1st number of the series are 0 and 1
@@ -12,11 +13,11 @@ int main() {
 	Fib[1] = 1;
 
 	//Fibonacci using DP
-	for(int i = 2; i <= n; i++) {
+	for(int i = 2; i < n; i++) {
 		Fib[i] = Fib[i - 1] + Fib[i - 2];
 	}
 
-	cout << Fib[n] << endl;
+	cout << Fib[n-1] << endl;
 
 	return 0;
 }
