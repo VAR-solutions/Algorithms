ea2d195590a95eb31ba9cad23debc388348a45df
edward cronin <edw.j.cronin@gmail.com>
2018-10-30 03:38:11 -0600

    reorganized /Bit Manipulation/Evenorodd & added my own .js folder/file

---
+++ Bit Manipulation/EvenOrOdd/Javascript/evenorodd.js
@@ -0,0 +1,15 @@
+var input;
+
+function evenorodd(input){
+  for(i = 0; i < input; i++){
+    if(i%2 == 0){
+      return(true);
+    }
+    else{
+      return(false);
+    }
+  }
+}
+
+evenorodd();
+
