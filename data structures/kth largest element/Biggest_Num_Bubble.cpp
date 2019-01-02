97a6835c2653deda1e35bb4c86c9b7a5b184d42b
Shivam Mittal <jmjmatter@gmail.com>
2019-01-02 18:47:23 +0530

    Updated, Done using Reverse Selection Sort
    The program now checks for kth largest element using reverse selection sort and better complexity.

--- data structures/kth largest element/Biggest_Num_Bubble.cpp
+++ data structures/kth largest element/Biggest_Num_Bubble.cpp
@@ -1,42 +1,44 @@
-// getting kth largest number in an array of numbers.
-// the complexity is O(N^2)
-
-#include <iostream>
+#include<iostream>
 using namespace std;
-void BubbleSort(int a[], int n){
-        int z,j,temp,flag; //temp will be used for swapping two consecutive numbers
-        for (z = 1; z <n; z++){
-            flag = 0;
-            for (j = 0 ; j < n-z ;j++){
-                if (a[j] > a[j+1]){
-                    temp = a[j];
-                    a[j]=a[j+1];
-                    a[j+1]= temp;
-                    flag = 1;
-                }
-            }
-            if (flag==0) //checks if sorted
-            break;
-        }
-    }
+void swap(int *x, int *y) 
+{ 
+    int temp = *x; 
+    *x = *y; 
+    *y = temp; 
+}
 
-int main(){
-    int k;
-    cout << "please enter the largest element order: ";
-    cin >> k;
-    int n;
-    cout << "Please enter the number of array elements: ";
-    cin >> n;
-    cout <<"Please enter the array elements one number in a line: \n";
-    int* a = new int[n]; // this is the line that allocates dynamically a place in memory with size n when the user enters n
-    for(int i=0;i<n;i++){
-        cin >> a[i]; //u can enter numbers separated by spaces or one number in each line
-    }
-    cout << "The sorted list is: \n";
-    BubbleSort(a, n);
-    for (int i=0;i<n; i++){
-            cout <<  a[i] << endl;
-    }
-    cout << "The largest required number is " << a[n-k]<<"."<< endl;
-    return 0;
+int main()
+{
+        int N, k, max;
+        cout << "Enter the Size Of the Arrray: ";
+        cin >> N;
+        int Arr[N];
+        cout << "\nEnter The Elements One by One: \n";
+        for(int i = 0; i < N; i++)
+        {
+                cin >> Arr[i];
+        }
+        cout << "\nEnter the value of k for which kth largest element is to be found: ";
+        cin >> k;
+        max = Arr[0];
+        if (k <= N)
+        {       
+        for (int j = 0; j < k; j++)
+        {
+                for (int l = 1; l < N; l++)
+                {
+                        if (max < Arr[l])
+                        {
+                                max = Arr[l];
+                        }
+                }
+                swap(max, Arr[j]);
+        }
+        }
+        else 
+        {
+                cout << "\n\nInvalid Value of K";
+        }
+        cout << "\n\nThe kth largest element is: " << Arr[k-1];
+        return 0;
 }
