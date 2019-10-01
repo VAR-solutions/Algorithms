7f95044720f4850631d7337c58c861e712223a46
AAYUSH DHEEMAR <31539600+Aayushdh99@users.noreply.github.com>
2019-10-01 13:16:48 +0530

    Create sizeOfStack.scala

---
+++ data structures/stack/scala/sizeOfStack.scala
@@ -0,0 +1,21 @@
+import scala.collection.mutable.Stack 
+  
+// Creating object 
+object GfG 
+{ 
+      
+    // Main method 
+    def main(args:Array[String]) 
+    { 
+  
+        var s = Stack[Int]() 
+  
+        s.push(5) 
+        s.push(1) 
+        s.push(2) 
+        println(s) 
+        println("Size: " + s.size) 
+        println("Popped:" + s.pop) 
+        println("Size: " + s.size) 
+    } 
+} 
