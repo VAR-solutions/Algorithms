52431f2e52d4bcded9bb0f38cc9ff37d7401883f
sumersingla <55348133+sumersingla@users.noreply.github.com>
2019-10-20 00:29:59 +0530

    Update random.c

--- Random Number/random.c
+++ Random Number/random.c
@@ -3,12 +3,12 @@
 #include <stdlib.h> 
   
 // Driver program and testing
-int main(void) 
+int main() 
 { 
     // This program will create same sequence of  
-    // random numbers on every program run  
-  
-    for(int i = 0; i<5; i++) 
+    // random numbers on every program run 
+    for(int i = 0; i<5; i++){ 
         printf(" %d ", rand()); 
+    }
     return 0; 
 } 
