9f14c306328ebaf7652168125255adb5e9d4699a
Jitendra Prajapati <jp803333@gmail.com>
2019-10-20 23:51:39 +0530

    Added reversing of singly linked list using recursion.

--- data structures/linked list/c/linked list.c
+++ data structures/linked list/c/linked list.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct SLLNode{
+struct SLLNode
+{
 	int data;
 	struct SLLNode *next;
 };
 
-struct DLLNode{
+struct DLLNode
+{
 	int data;
 	struct DLLNode *prev;
 	struct DLLNode *next;
@@ -15,164 +17,201 @@ struct DLLNode{
 typedef struct DLLNode DLL_NODE;
 typedef struct SLLNode SLL_NODE;
 
- SLL_NODE* makeNewSLLNode(int data){
-	SLL_NODE *newSLLNode = (SLL_NODE *) calloc(1, sizeof(SLL_NODE));
+SLL_NODE *makeNewSLLNode(int data)
+{
+	SLL_NODE *newSLLNode = (SLL_NODE *)calloc(1, sizeof(SLL_NODE));
 	newSLLNode->data = data;
 	return newSLLNode;
 }
 
-void SLLInsert(SLL_NODE **head, int data, int position){
+void SLLInsert(SLL_NODE **head, int data, int position)
+{
 	SLL_NODE *current = *head;
 	int k = 1;
-	
+
 	SLL_NODE *newSLLNode = makeNewSLLNode(data);
-	if(position == 1){
+	if (position == 1)
+	{
 		newSLLNode->next = current;
 		(*head) = newSLLNode;
 		return;
 	}
-	
-	while((k < position - 1) && (current->next !=NULL)){
+
+	while ((k < position - 1) && (current->next != NULL))
+	{
 		current = current->next;
 		k++;
 	}
-	
+
 	newSLLNode->next = current->next;
 	current->next = newSLLNode;
-	
 }
 
-void SLLDelete(SLL_NODE **head, int position){
+void SLLDelete(SLL_NODE **head, int position)
+{
 	SLL_NODE *temp = *head;
 	int k = 1;
-	
+
 	SLL_NODE *temp2;
-	
-	if(position == 1){
+
+	if (position == 1)
+	{
 		(*head) = (*head)->next;
 		free(temp);
 		return;
 	}
-	
-	while((k < position) && (temp != NULL)){
+
+	while ((k < position) && (temp != NULL))
+	{
 		temp2 = temp;
 		temp = temp->next;
 		k++;
 	}
-	
+
 	temp2->next = temp->next;
 	free(temp);
-	
 }
 
-void deleteSingleLinkedList(SLL_NODE **head){
+void deleteSingleLinkedList(SLL_NODE **head)
+{
 	SLL_NODE *current = *head;
-	while(current != NULL){
+	while (current != NULL)
+	{
 		(*head) = (*head)->next;
 		free(current);
 		current = *head;
 	}
 }
 
-void displaySLL(SLL_NODE *head){
-	SLL_NODE* curr = head;
+void displaySLL(SLL_NODE *head)
+{
+	SLL_NODE *curr = head;
 	printf("\nPrinting the list : ");
-	while(curr != NULL){
+	while (curr != NULL)
+	{
 		printf("%d -> ", curr->data);
 		curr = curr->next;
 	}
 }
 
-DLL_NODE* makeNewDLLNode(int data){
-	DLL_NODE *newNode = (DLL_NODE*) calloc(1, sizeof(DLL_NODE*));
+DLL_NODE *makeNewDLLNode(int data)
+{
+	DLL_NODE *newNode = (DLL_NODE *)calloc(1, sizeof(DLL_NODE *));
 	newNode->data = data;
 	return newNode;
 }
 
-void DLLInsert(DLL_NODE **head, int data , int position){
-	DLL_NODE* newNode = makeNewDLLNode(data);
-	DLL_NODE* temp = *head;
-	
+void DLLInsert(DLL_NODE **head, int data, int position)
+{
+	DLL_NODE *newNode = makeNewDLLNode(data);
+	DLL_NODE *temp = *head;
+
 	int k = 1;
-	if(position == 1){
+	if (position == 1)
+	{
 		newNode->prev = NULL;
 		newNode->next = *head;
-		*head = newNode;	
-		return;	
+		*head = newNode;
+		return;
 	}
-	
-	while((k < position - 1) && (temp->next != NULL)){
+
+	while ((k < position - 1) && (temp->next != NULL))
+	{
 		temp = temp->next;
 		k++;
 	}
-	
+
 	newNode->next = temp->next;
 	newNode->prev = temp;
-	if(temp->next){
+	if (temp->next)
+	{
 		temp->next->prev = newNode;
 	}
-	
-	temp->next = newNode;
 
+	temp->next = newNode;
 }
 
-void DLLDelete(DLL_NODE **head, int position){
+void DLLDelete(DLL_NODE **head, int position)
+{
 	DLL_NODE *temp = *head;
 	DLL_NODE *temp2;
-	
+
 	int k = 1;
-	
-	 if(*head == NULL){
-	 	printf("Empty List");
-	 	return;
-	 }
-	 
-	 if(position == 1){
-	 	temp = *head;
-	 	(*head) = (*head)->next;
-	 	free(temp);
-	 	return;
-	 }
-	 
-	 while((k < position) && (temp->next != NULL)){
-	 	temp = temp->next;
-	 	k++;
-	 }
-	 
-	 temp2 = temp->prev;
-	 temp->next->prev = temp2;
-	 temp2->next = temp->next;
-	 free(temp);
-	 return; 
-	 
+
+	if (*head == NULL)
+	{
+		printf("Empty List");
+		return;
+	}
+
+	if (position == 1)
+	{
+		temp = *head;
+		(*head) = (*head)->next;
+		free(temp);
+		return;
+	}
+
+	while ((k < position) && (temp->next != NULL))
+	{
+		temp = temp->next;
+		k++;
+	}
+
+	temp2 = temp->prev;
+	temp->next->prev = temp2;
+	temp2->next = temp->next;
+	free(temp);
+	return;
 }
 
-void displayDLL(DLL_NODE* head){
-	DLL_NODE* current = head;
+void displayDLL(DLL_NODE *head)
+{
+	DLL_NODE *current = head;
 	printf("\n");
-	while(current != NULL){
+	while (current != NULL)
+	{
 		printf("%d -> ", current->data);
 		current = current->next;
 	}
 }
 
-void singlyLinkedList() {
+void reverseSLL(SLL_NODE **head, SLL_NODE *temp)
+{
+	//reverse the singly linked list using recursion.
+	if (*head == NULL)
+	{
+		return;
+	}
+	if (temp->next == NULL)
+	{
+		*head = temp;
+		return;
+	}
+	reverseSLL(head, temp->next);
+	temp->next->next = temp;
+	temp->next = NULL;
+}
+void singlyLinkedList()
+{
 	SLL_NODE *head = NULL;
 	SLLInsert(&head, 7, 1);
-	SLLInsert(&head, 6, 1); 
+	SLLInsert(&head, 6, 1);
 	SLLInsert(&head, 5, 1);
 	SLLInsert(&head, 4, 1);
 	SLLInsert(&head, 3, 1);
 	SLLInsert(&head, 1, 1);
 	SLLInsert(&head, 2, 2);
 	displaySLL(head);
-	
+	reverseSLL(&head,head);
+	displaySLL(head);
 	SLLDelete(&head, 4);
 	displaySLL(head);
 }
 
-void doublyLinkedList() {
-	DLL_NODE* head = NULL;
+void doublyLinkedList()
+{
+	DLL_NODE *head = NULL;
 	DLLInsert(&head, 7, 1);
 	DLLInsert(&head, 6, 1);
 	DLLInsert(&head, 4, 1);
@@ -187,24 +226,24 @@ void doublyLinkedList() {
 
 int main()
 {
-    int a;
+	int a;
 
-    printf("Choose the type of list: ");
-    printf("\n1. Singly linked list\n2. Doubly linked list\n");
-    printf("Your choice:  ");
-    scanf("%d", &a);
-    switch (a)
-    {
-      case 1:
-           singlyLinkedList();
-          break;
-      case 2:
-          doublyLinkedList();
-          break;
-      default:
-          printf("Invalid choice !!!");
-          break;
-    }
+	printf("Choose the type of list: ");
+	printf("\n1. Singly linked list\n2. Doubly linked list\n");
+	printf("Your choice:  ");
+	scanf("%d", &a);
+	switch (a)
+	{
+	case 1:
+		singlyLinkedList();
+		break;
+	case 2:
+		doublyLinkedList();
+		break;
+	default:
+		printf("Invalid choice !!!");
+		break;
+	}
 
-    return 0;
+	return 0;
 }
