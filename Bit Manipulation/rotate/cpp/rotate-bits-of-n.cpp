e0a355181fee7357bdf4a7c04604c420e34b08a7
quackbla <speed4runs@hotmail.it>
2019-10-28 23:30:00 +0100

    Fixed undefined behaviour, removed redundancies, made the file linkable and compilable.

--- Bit Manipulation/rotate/cpp/rotate-bits-of-n.cpp
+++ Bit Manipulation/rotate/cpp/rotate-bits-of-n.cpp
@@ -3,18 +3,18 @@
 #define INT_BITS 32
 
 int leftRotate(int n, unsigned int d){
-   return (n << d)|(n >> (INT_BITS - d));
+   return n << d%INT_BITS;
 }
 
 int rightRotate(int n, unsigned int d){
-   return (n >> d)|(n << (INT_BITS - d));
+   return n >> d%INT_BITS;
 }
 
 int main(){
   int n,d;
   //input n (number) and d (no. of bits)
-  cin>>n>>d;
-  cout<<"Left Rotation of "<<n<<" by "<<d<<" is "<<leftRotate(n,d) <<endl;
-  cout<<"Right Rotation of "<<n<<" by "<<d<<" is "<<rightRotate(n,d) <<endl
+  std::cin>>n>>d;
+  std::cout<<"Left Rotation of "<<n<<" by "<<d<<" is "<<leftRotate(n,d) <<std::endl;
+  std::cout<<"Right Rotation of "<<n<<" by "<<d<<" is "<<rightRotate(n,d) <<std::endl;
   return 0;
 }
