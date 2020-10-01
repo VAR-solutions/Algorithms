7e5f37643f93be9db0b55b3f497c51b5b505fe2c
Abhishek Garain <abhi211199@gmail.com>
2020-10-01 16:11:09 +0530

    added arduino to nodejs interface

---
+++ Arduino_Code/Arduino-NodeJS/write_data_tp_serial.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.write("hello from arduino\n");
+  delay(2000);
+}
+
