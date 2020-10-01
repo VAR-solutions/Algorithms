188cf0b28ed6c9c5555529e62eb95ca3dd080b33
sumitisnot4u <56196142+sumitisnot4u@users.noreply.github.com>
2020-10-01 12:04:32 +0530

    Update bubble_sort.cpp

--- Sorting/Bubble Sort/C++/bubble_sort.cpp
+++ Sorting/Bubble Sort/C++/bubble_sort.cpp
@@ -1,47 +1,41 @@
-/* Bubble Sort implementation in C++
- * Author : Manvi Gupta
- * Input : array length and elements
- * Output : Sorted array elements
- */
-#include <iostream>
-using namespace std;
+// C program for implementation of Bubble sort 
+#include <stdio.h> 
 
-int n;
+void swap(int *xp, int *yp) 
+{ 
+	int temp = *xp; 
+	*xp = *yp; 
+	*yp = temp; 
+} 
 
-void bubble_Sort(int a[])
-{
+// A function to implement bubble sort 
+void bubbleSort(int arr[], int n) 
+{ 
+int i, j; 
+for (i = 0; i < n-1; i++)	 
 
- 	for(int i=0; i<n-1; i++)
- 	{
- 		int flag = 0;  // Declaring a variable to check if elements have been sorted in this iteration.
- 		for(int j=0; j<n-1-i; j++)
- 		{
- 			if(a[j] > a[j+1])
- 			{
-   			swap(a[j+1], a[j]);
-   			flag = 1;		// Set flag = 1 to signify that the element is sorted.c
- 			}
- 		}	
- 		if (flag == 0)
- 		{
- 			// No elements have been swapped, which means that the array is sorted. End the algorithm.
- 			break;
- 		}
- 	}
-}
+	// Last i elements are already in place 
+	for (j = 0; j < n-i-1; j++) 
+		if (arr[j] > arr[j+1]) 
+			swap(&arr[j], &arr[j+1]); 
+} 
 
-int main()
-{
-	cout<<"Enter the size of the array"<<endl;
-	cin >> n;
-	int a[n];
-	cout<<"Enter the elements of the array"<<endl;
-	for(int i=0; i<n; i++)
-	cin >> a[i];
-	bubble_Sort(a);
-	cout<<"The sorted array is : "<<endl;
-	for (int i = 0; i < n; i++) {
-		cout << a[i] << '\n';
-  }
-  return 0;
-}
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+	int i; 
+	for (i=0; i < size; i++) 
+		printf("%d ", arr[i]); 
+	printf("\n"); 
+} 
+
+// Driver program to test above functions 
+int main() 
+{ 
+	int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	bubbleSort(arr, n); 
+	printf("Sorted array: \n"); 
+	printArray(arr, n); 
+	return 0; 
+} 
