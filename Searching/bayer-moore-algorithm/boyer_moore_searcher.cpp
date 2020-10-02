188c71050c6d75deb3ee39d032efc69f681fd449
Nexus Web Development <github@nexwebsites.com>
2020-10-02 04:07:43 -0400

    Create boyer_moore_searcher.cpp
    #163

---
+++ Searching/bayer-moore-algorithm/boyer_moore_searcher.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+ 
+int main()
+{
+    std::string in = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    std::string needle = "aliquip";
+    auto it = std::search(in.begin(), in.end(),
+                   std::boyer_moore_searcher(
+                       needle.begin(), needle.end()));
+    if(it != in.end())
+        std::cout << "The string " << needle << " found at offset "
+                  << it - in.begin() << '\n';
+    else
+        std::cout << "The string " << needle << " not found\n";
+}
