c1d2985a392391e29d12954fd5975672a39ced81
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-10-02 13:03:31 +0530

    Update Armstrong_no.java

--- Mathematics/Armstrong_no/Java/Armstrong_no.java
+++ Mathematics/Armstrong_no/Java/Armstrong_no.java
@@ -1,5 +1,14 @@
 /*
 
+An armstrong number is a special number where in if all digits of the number are cubed and added, it results in the original number
+example:
+    153 = 1*1*1 + 5*5*5 + 3*3*3 
+    370 = 3*3*3 + 7*7*7 + 0*0*0
 
+The usage of Armstrong numbers are ususlly in data security systems.
+
+The following java program helps us to find whether a number inputted by a user is an armstrong number or not.
 
 */
+
+
