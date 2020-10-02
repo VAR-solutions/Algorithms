fd3a444c7ededab81ad17a1362dc48405ab42698
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-10-02 13:23:05 +0530

    Update fibonacci.java

--- Mathematics/fibonacci/java/fibonacci.java
+++ Mathematics/fibonacci/java/fibonacci.java
@@ -1,20 +1,46 @@
-import java.util.*;
-import java.io.*;
-class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int a = 0;
-    int b = 1;
-    if(n==0) {
-      System.out.println(a);
-      return;
-    }
-    for(int i = 2; i<=n; i++) {
-      int c = a+b;
-      a = b;
-      b = c;
-    }
-    System.out.println(b);
-  }
-}
+/*
+
+A fibonacci series is a special series wherein every number (except for 0 and 1) is a sum of the previous 2 numbers.
+example:
+    0 1 1 2 3 5 8 .....
+    here,
+        8 = 5+3
+        5 = 3+2
+        3 = 2+1
+A fibonacci series forms the basis of the geometric pattern that the universe forms into. Its relevance comes into play when we aggregate the series to find it matching to the golden ratio
+
+The following program is an efficient way of finding the fibonacci series upto the Nth term via dynamic programming
+*/
+
+class fibonacci { 
+
+	// Function to find the fibonacci series 
+	static int fib(int n) 
+	{ 
+		int f[] = new int[n + 2];      // To store the fibonacci numbers
+		int i; 
+    
+		f[0] = 0; 
+		f[1] = 1;     // initial numbers of the fibonacci series (pre known)
+
+		for (i = 2; i <= n; i++) { 
+
+			// Add the previous 2 numbers 
+			// in the series and store it 
+			f[i] = f[i - 1] + f[i - 2]; 
+		} 
+
+		// Nth Fibonacci Number 
+		return f[n]; 
+	} 
+
+	public static void
+	main(String args[]) 
+	{ 
+		int N = 10; // taking N as 10. It can be inputted via user as well
+
+		// Print first 10 term 
+		for (int i = 0; i < N; i++) 
+			System.out.print(fib(i) + " "); 
+	} 
+} 
