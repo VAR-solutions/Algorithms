637d45af2e097f3761c437622da4c89730bbef69
karthikeyanrathore <you@example.com>
2020-10-02 13:04:56 +0530

    Added new chanhes

--- Sorting/Bubble Sort/Shell/bubble_sort.sh
+++ Sorting/Bubble Sort/Shell/bubble_sort.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Bubble Sort :: Time : O(n ^ 2)
+
+
 # Creating an array
 a=(200 100 300 700 600 500)
-n=6
+n=6 
 
 echo "Array Before Sorting: "
 echo ${a[*]}
 
+
 for ((i = 0; i < n; ++i))
 do
+	# initialized boolean check as false
+	check=false
 	for ((j = 0; j < n - i - 1; ++j))
 	do
 		if [ ${a[j]} -gt ${a[$((j + 1))]} ]
@@ -16,12 +22,40 @@ do
 			temp=${a[j]}
 			a[$j]=${a[$((j + 1))]}
 			a[$((j + 1))]=$temp
+			
+			# Swap occurred in inner loop 
+			check=true
 		fi
 	done
+	
+	# if no two elements were swapped by inner loop  , then break 
+	if [ "$check" = false ] ; then
+		break
+	fi
 done
 
 echo
 echo "Array After Sorting:"
 echo ${a[*]}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
