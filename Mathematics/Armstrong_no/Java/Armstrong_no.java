24f55d21dbd7ab003b2a6b8aeb0836c6b34909a6
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-10-02 13:14:42 +0530

    Update Armstrong_no.java

--- Mathematics/Armstrong_no/Java/Armstrong_no.java
+++ Mathematics/Armstrong_no/Java/Armstrong_no.java
@@ -11,4 +11,48 @@ The following java program helps us to find whether a number inputted by a user 
 
 */
 
+import java.util.*;
 
+class Armstrong_no
+{
+    
+    boolean checkArmstrong(int number)
+    {
+        int temp = number;  // to store a temporary copy of the number
+        int sum = 0;        // to store the sum of the cube of the digits
+        while(temp > 0)
+        {
+            int last_digit = temp%10;   // to get the last digit of 'number'
+            sum += last_digit * last_digit * last_digit;
+            temp /= 10;                 // removing the last digit by truncating the number
+        }
+        
+        if(sum == number)
+            return true;
+        else
+            return false;
+    }
+    
+    
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a number : ");
+        int number = sc.nextInt();
+        
+        Armstrong_no object = new Armstrong_no();
+        if(object.checkArmstrong(number))
+            System.out.println("The number " + number + " is an Armstrong Number");
+        else
+            System.out.println("The number " + number + " is not an Armstrong Number");
+    }
+}
+
+
+
+
+// Test this code on the following numbers to see the results : 
+//  153
+//  407
+//  1571
+//  1634
