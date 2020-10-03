8c9a8700521d87163b1854d10e5b20d01b171546
unknown <mohitmallick17@gmail.com>
2020-10-03 17:24:44 +0530

    Add toggle to switch between Max-Heap/Min-Heap,Fixing some logical bugs & Modularize lchild/rchild formulas

--- Sorting/Heapsort/C++/heapsort.cpp
+++ Sorting/Heapsort/C++/heapsort.cpp
@@ -1,29 +1,50 @@
 // C++ program for implementation of Heap Sort
 #include <iostream>
+#define MIN_HEAP 0;
+#define MAX_HEAP 1;
 
 using namespace std;
+
+// Choice to choose between Max/Min Heap.
+int CHOICE = MAX_HEAP;
+//Use MIN_HEAP to use this algorithm as a Min Heap. 
+
+// We defined a comparator function to automatically make comparisions based on our Choice instead of making it generic
+bool const comparator(const int l, const int r) {
+    if (CHOICE)
+        return l > r;
+    return l < r;
+}
+
+// We use these function calls lchild() and rchild() to get the left child/right child for any node
+int lchild(const int i) {
+    return 2 * i;
+}
+
+int rchild(const int i) {
+    return 2 * i + 1;
+}
 
 // To heapify a subtree rooted with node i which is
 // an index in arr[]. n is size of heap
 void heapify(int arr[], int n, int i)
 {
     int largest = i; // Initialize largest as root
-    int l = 2*i + 1; // left = 2*i + 1
-    int r = 2*i + 2; // right = 2*i + 2
+    int l = lchild(i); // left = 2*i + 1
+    int r = rchild(i); // right = 2*i + 2
 
-    // If left child is larger than root
-    if (l < n && arr[l] > arr[largest])
+    // compare left child & root
+    if (l < n && comparator(arr[l], arr[largest]))
         largest = l;
 
-    // If right child is larger than largest so far
-    if (r < n && arr[r] > arr[largest])
+    // compare right child & root
+    if (r < n && comparator(arr[r], arr[largest]))
         largest = r;
 
     // If largest is not root
     if (largest != i)
     {
         swap(arr[i], arr[largest]);
-
         // Recursively heapify the affected sub-tree
         heapify(arr, n, largest);
     }
@@ -37,7 +58,7 @@ void heapSort(int arr[], int n)
         heapify(arr, n, i);
 
     // One by one extract an element from heap
-    for (int i=n-1; i>=0; i--)
+    for (int i = n - 1; i >= 0; i--)
     {
         // Move current root to end
         swap(arr[0], arr[i]);
@@ -50,7 +71,7 @@ void heapSort(int arr[], int n)
 /* A utility function to print array of size n */
 void printArray(int arr[], int n)
 {
-    for (int i=0; i<n; ++i)
+    for (int i = 0; i < n; ++i)
         cout << arr[i] << " ";
     cout << "\n";
 }
@@ -58,11 +79,11 @@ void printArray(int arr[], int n)
 // Driver program
 int main()
 {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    int n = sizeof(arr) / sizeof(arr[0]);
 
     heapSort(arr, n);
 
     cout << "Sorted array is \n";
     printArray(arr, n);
-} 
+}
