f4fbc6b486e4306117ba4a2d38ab8875e1af5c25
justyy <dr.zhihua.lai@gmail.com>
2020-10-03 19:18:21 +0100

    Using iterative approach O(1) constant

--- Dynamic Programming/fibo.cpp
+++ Dynamic Programming/fibo.cpp
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int Fib[10000];
-
 int main() {
 	int n;
 	cin >> n;
 
 	//0th and 1st number of the series are 0 and 1
-	Fib[0] = 0;
-	Fib[1] = 1;
+	int a = 0;
+	int b = 1;
 
 	//Fibonacci using DP
 	for(int i = 2; i <= n; i++) {
-		Fib[i] = Fib[i - 1] + Fib[i - 2];
+		int c = a + b;
+		a = b;
+		b = c;
 	}
 
-	cout << Fib[n] << endl;
+	cout << a << endl;
 
 	return 0;
 }
