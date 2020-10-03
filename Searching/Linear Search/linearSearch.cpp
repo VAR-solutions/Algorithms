82da2d266e968ec608355a046c5a99a6791fa139
Kailash_mms <srikarkailash9@gmail.com>
2020-10-03 17:45:28 +0530

    update linearsearch.cpp

--- Searching/Linear Search/linearSearch.cpp
+++ Searching/Linear Search/linearSearch.cpp
@@ -1,19 +1,21 @@
 #include<iostream>
 using namespace std;
-int pos(int arr[] , int size , int tofind)
-{
-	
-	for(int i=0;i<size;i++)
-		if (arr[i] == tofind) return i;
-	return -1;
-}
-int main()
-{
-	const int size = 5;
-	int arr[] = {1,2,3,4,5};
-	int p = pos(arr, size, 2);
-	if (p == -1)
-		cout <<"Not found\n";
-	else cout << "Found at position "<<p+1<<endl;
-return 0;
+int main(){
+	int i, j, n, search;
+	cin >> n;
+	int arr[n]; //Declaration
+	for(i=0;i<n;i++){
+		cin >> arr[i]; //Input Elements Into Array 
+	}
+	cin >> search; //Inputting the search element
+	for(i=0;i<n;i++){
+		if(arr[i] == search){
+			cout << "Element Found at the position " << i+1 << endl; //Linear Search Algorithm
+			break;
+		}
+	}
+	if(i == n){
+		cout << "Element Not Found"; //In case if element is not there
+	}
+	return 0;
 }
