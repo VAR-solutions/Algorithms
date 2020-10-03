6a2dce355077dac64dc288720209412b664712b3
justyy <dr.zhihua.lai@gmail.com>
2020-10-03 19:22:42 +0100

    Iterative approach to compute the GCD
    Taken from https://helloacm.com/smallest-multiple-algorithm-using-bruteforce-or-gcd-lcm/

--- Mathematics/GCD/cpp/FindGCD.cpp
+++ Mathematics/GCD/cpp/FindGCD.cpp
@@ -8,7 +8,17 @@ int gcd(int a, int b){
         return a;
     return gcd(b, a % b);
 }
- 
+
+// iterative approach
+int gcd2(int a, int b) {
+    while (b != 0) {
+        let c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;
+}
+	
 int main(){
     int a, b;
 	cin>>a>>b;
