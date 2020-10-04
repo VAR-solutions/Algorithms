22f00b6f5a5bf75b526397be35795490e0c422f5
Mitali-laroia <04042001mitali@gmail.com>
2020-10-04 11:42:59 +0530

    Kedane in C#

---
+++ Other Algorithms/Kadane Algorithm/C#/Kedane.cs
@@ -0,0 +1,37 @@
+// C# program to print largest  
+// contiguous array sum 
+using System; 
+  
+class Kedane
+{ 
+    static int maxSubArraySum(int []a) 
+    { 
+        int size = a.Length; 
+        int max_so_far = int.MinValue,  
+            max_ending_here = 0; 
+  
+        for (int i = 0; i < size; i++) 
+        { 
+            max_ending_here = max_ending_here + a[i]; 
+              
+            if (max_so_far < max_ending_here) 
+                max_so_far = max_ending_here; 
+              
+            if (max_ending_here < 0) 
+                max_ending_here = 0; 
+        } 
+          
+        return max_so_far; 
+    } 
+   
+    public static void Main () 
+    { 
+        int [] a = {-2, -3, 4, -1, -2, 1, 5, -3}; 
+        Console.Write("Maximum contiguous sum is " + 
+                                maxSubArraySum(a)); 
+    } 
+  
+} 
+
+
+
